933d5e14981fcdbd0e2d129379c4b195a5ffeb07
Wilbur <wangenbo@aliyun.com>
2020-04-15 10:44:38 +0800

    fix: 修复使用node 操作mysql 案例不能请求的问题

--- FE/demo/mysql-test/mysql.js
+++ FE/demo/mysql-test/mysql.js
@@ -4,7 +4,7 @@ const Router = require('koa-router');
 const Convert = require('koa-convert');
 const static = require('koa-static');
 
-
+const PAGE_SIZE = 5;
 
 //1. 连接数据库
 let db = mysql.createPool({
@@ -15,9 +15,8 @@ let db = mysql.createPool({
 });
 
 function query(sql) {
-    new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         db.query(sql, (err, rows)=>{
-            console.log(rows);
             if(err) {
                 reject(err);
             }else{
@@ -30,13 +29,36 @@ function query(sql) {
 let app = new Koa();
 let router = new Router();
 
+router.get('/', async ctx=> {
+    ctx.body = "Hello World";
+});
+
 router.get('/get_user', async ctx=> {
     //2. 操作数据库 SQL
-    let rows = await query('SELECT * FROM api_user');
+    const {page = 1, channel} = ctx.query;
 
-    ctx.body = rows;
-})
+    if(channel) {
+        let datas = await query(`SELECT ID FROM api_user WHERE channel='${channel}' LIMIT ${(page - 1) * PAGE_SIZE}, ${PAGE_SIZE}`);
+        let rows = await query(`SELECT count(*) AS count FROM api_user WHERE channel='${channel}'`);
+        let total = Math.ceil(rows[0].count / PAGE_SIZE);
 
+        ctx.body = {
+            datas,
+            total
+        };
+    }else{
+        let datas = await query(`SELECT ID FROM api_user LIMIT ${(page - 1) * PAGE_SIZE}, ${PAGE_SIZE}`);
+        let rows = await query(`SELECT count(*) AS count FROM api_user`);
+        let total = Math.ceil(rows[0].count / PAGE_SIZE);
+
+        ctx.body = {
+            datas,
+            total
+        };
+    }
+
+
+});
 
 app.use(router.routes());
 app.use(static('./static'));
@@ -44,5 +66,3 @@ app.use(static('./static'));
 app.listen(1234, ()=>{
     console.log('server listening at 1234');
 });
-
-
